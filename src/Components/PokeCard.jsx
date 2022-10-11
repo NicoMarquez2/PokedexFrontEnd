9119aea5b3d0e2b6787fea915728aad711ae70bb
ignacio gaston odella <108488609+Nano92OB@users.noreply.github.com>
2022-10-11 20:35:08 -0300

    index pokemon

--- src/Components/PokeCard.jsx
+++ src/Components/PokeCard.jsx
@@ -5,148 +5,248 @@ import { Link } from "react-router-dom";
 
 const PokeCard = (props) => {
   const [pokemon, setpokemon] = useState();
+  const [pokemonIdx, setpokemonIdx] = useState();
   const params = useParams();
   const id = params.id;
 
-  
   useEffect(() => {
-    setpokemon(props.list.find((element) => element.id == id));
-    
-  }, [props.list]);
- 
+    const pokemon = props.list.find((element, idx) => {
+      setpokemonIdx(idx);
+      return element.id == id;
+    });
+    setpokemon(pokemon);
+  }, [props.list, id]);
 
-   function fixId(pokeId){
-      if (pokeId < 10){
-          return "00"+pokeId
-      }
-      else if (pokeId >= 10 && pokeId < 100){
-          return "0"+pokeId
-      }
-      else{
-          return pokeId
-      }
-   }
+  function fixId(pokeId) {
+    if (pokeId < 10) {
+      return "00" + pokeId;
+    } else if (pokeId >= 10 && pokeId < 100) {
+      return "0" + pokeId;
+    } else {
+      return pokeId;
+    }
+  }
 
-   function fixStats(pokeStats){
-    if(pokeStats < 100)
-        return "0"+pokeStats
-    else
-        return pokeStats
-   }
-
+  function fixStats(pokeStats) {
+    if (pokeStats < 100) return "0" + pokeStats;
+    else return pokeStats;
+  }
 
   return (
     <React.Fragment>
       {pokemon && (
-      <div className={`pokeCardOnly ${(typeof pokemon.type==="string") ? pokemon.type : pokemon.type[0]}`}>      
-            <div className="cardTitle">
-              <div>
-                <img src="/Referencias/Pokeball.png" className="pokebackground"/>
+        <div
+          className={`pokeCardOnly ${
+            typeof pokemon.type === "string" ? pokemon.type : pokemon.type[0]
+          }`}
+        >
+          <div className="cardTitle">
+            <div>
+              <img src="/Referencias/Pokeball.png" className="pokebackground" />
+            </div>
+            <div className="header">
+              <div className="lettersWhite">
+                <Link to={"/"}>
+                  <img src="/Referencias/arrow-left.svg" className="arrow" />
+                </Link>
+
+                <span className="pokeName">{pokemon.name}</span>
               </div>
-              <div className="header">
-                <div className="lettersWhite">
-                  <Link to={'/'}>
-                    <img src="/Referencias/arrow-left.svg" className="arrow"/>   
-                  </Link>
-                         
-                  <span className="pokeName" >{pokemon.name}</span> 
-                </div>       
-                <div className="lettersWhite bold"><span>#{fixId(pokemon.id)}</span></div>
-              </div> 
-              <div className="navigate">
-                <img className="rotate" src="/Referencias/Frame.svg" alt="#" />
+              <div className="lettersWhite bold">
+                <span>#{fixId(pokemon.id)}</span>
+              </div>
+            </div>
+            <div className="navigate">
+              <Link to={`/${props.list[pokemonIdx-1].id}`}>
+              <img className="rotate" src="/Referencias/Frame.svg" alt="#" />
+              </Link>
+              <Link to={`/${props.list[pokemonIdx+1].id}`}>
                 <img src="/Referencias/Frame.svg" alt="#" />
-              </div>          
+              </Link>
+            </div>
+          </div>
+
+          <img
+            className="imgPokeCard"
+            src={`Referencias/${pokemon.name}.png`}
+          />
+
+          <div className="pokeDescription">
+            <div className="pokeType">
+              {typeof pokemon.type === "string" ? (
+                <div>
+                  <span
+                    className={`singleType ${pokemon.type} lettersWhite typeTitle`}
+                  >
+                    {pokemon.type}
+                  </span>
+                </div>
+              ) : (
+                <div>
+                  <span
+                    className={`dobleType1 ${pokemon.type[0]} lettersWhite`}
+                  >
+                    {pokemon.type[0]}
+                  </span>
+                  <span
+                    className={`dobleType2 ${pokemon.type[1]} lettersWhite`}
+                  >
+                    {pokemon.type[1]}
+                  </span>
+                </div>
+              )}
             </div>
 
-            <img className="imgPokeCard" src={`Referencias/${pokemon.name}.png`}/>
-            
-            <div className="pokeDescription">
-                <div className="pokeType">
-                  {(typeof pokemon.type==="string") ? 
-                    <div><span className={`singleType ${pokemon.type} lettersWhite typeTitle`}>{pokemon.type}</span></div> :
+            <div className="pokeAbout">
+              <span
+                className={`subtitle ${
+                  typeof pokemon.type === "string"
+                    ? pokemon.type
+                    : pokemon.type[0]
+                } backgroundWhite`}
+              >
+                About
+              </span>
+              <div className="pokeInfo">
+                <div className="categoryLeft">
+                  <div>
+                    <img
+                      src="/Referencias/Weight.svg"
+                      alt="#"
+                      className="imgCategory"
+                    />
+                    <span className="spanCategory">{pokemon.weigth} Kg</span>
+                  </div>
 
+                  <span className="categoryTitle">Weight</span>
+                </div>
+                <hr></hr>
+                <div className="category">
+                  <div>
+                    <img
+                      src="/Referencias/Height.svg"
+                      alt="#"
+                      className="imgCategory"
+                    />
+                    <span className="spanCategory">{pokemon.heigth} m</span>
+                  </div>
+
+                  <span className="categoryTitle">Heigth</span>
+                </div>
+                <hr></hr>
+
+                <div className="categoryRigth">
+                  {typeof pokemon.moves === "string" ? (
                     <div>
-                      <span className={`dobleType1 ${pokemon.type[0]} lettersWhite`}>{pokemon.type[0]}</span>
-                      <span className={`dobleType2 ${pokemon.type[1]} lettersWhite`}>{pokemon.type[1]}</span>
-                    </div>}                
-                </div>
-              
-                <div className="pokeAbout">
-                  <span className={`subtitle ${(typeof pokemon.type==="string") ? pokemon.type : pokemon.type[0]} backgroundWhite`}>About</span>
-                  <div className="pokeInfo">
-                    <div className="categoryLeft">
-                      <div>
-                        <img src="/Referencias/Weight.svg" alt="#" className="imgCategory"/>
-                        <span className="spanCategory">{pokemon.weigth} Kg</span>
-                      </div>
-                      
-                      <span className="categoryTitle">Weight</span>
+                      <span>{pokemon.moves}</span>
                     </div>
-                    <hr></hr>
-                    <div className="category">
-                      <div>
-                        <img src="/Referencias/Height.svg" alt="#" className="imgCategory"/>
-                        <span className="spanCategory">{pokemon.heigth} m</span>  
-                      </div>
-                      
-                      <span className="categoryTitle">Heigth</span>
-                    </div>
-                    <hr></hr>
-
-                    <div className="categoryRigth">
-                    {(typeof pokemon.moves==="string") ? 
-                    <div><span>{pokemon.moves}</span></div> :
-
+                  ) : (
                     <div className="prueba">
-                      <span className={`${pokemon.moves[0]}`}>{pokemon.moves[0]}</span>
-                      <span className={`${pokemon.moves[1]}`}>{pokemon.moves[1]}</span>
-                    </div>}  
-                    <span className="categoryTitle">Moves</span>            
-                    </div>                          
-                  </div>
-
-                </div>
-
-                <div className="pokeSummary">
-                  <span>{pokemon.description}</span>
-                </div>
-
-                <div className="pokeStats">
-                  <div><span className={ ` subtitle2 ${(typeof pokemon.type==="string") ? pokemon.type : pokemon.type[0]} backgroundWhite`}>Base Stats</span></div>                  
-                  <div className="statsFlex">
-                    <div className="nameAndNumber">
-                      <div className="stats">
-                        <span className={`${(typeof pokemon.type==="string") ? pokemon.type : pokemon.type[0]} backgroundWhite`}>HP</span>
-                        <span className={`${(typeof pokemon.type==="string") ? pokemon.type : pokemon.type[0]} backgroundWhite`}>ATK</span>  
-                        <span className={`${(typeof pokemon.type==="string") ? pokemon.type : pokemon.type[0]} backgroundWhite`}>DEF</span>
-                        <span className={`${(typeof pokemon.type==="string") ? pokemon.type : pokemon.type[0]} backgroundWhite`}>SATK</span>
-                        <span className={`${(typeof pokemon.type==="string") ? pokemon.type : pokemon.type[0]} backgroundWhite`}>SDEF</span> 
-                        <span className={`${(typeof pokemon.type==="string") ? pokemon.type : pokemon.type[0]} backgroundWhite`}>SPD</span>             
-                      </div>
-                      <hr />
-                      <div className="stats">
-                        <span>{fixStats(pokemon.hp)}</span>
-                        <span>{fixStats(pokemon.atk)}</span>
-                        <span>{fixStats(pokemon.def)}</span>
-                        <span>{fixStats(pokemon.satk)}</span>
-                        <span>{fixStats(pokemon.sdef)}</span>
-                        <span>{fixStats(pokemon.spd)}</span>
-                      </div>
+                      <span className={`${pokemon.moves[0]}`}>
+                        {pokemon.moves[0]}
+                      </span>
+                      <span className={`${pokemon.moves[1]}`}>
+                        {pokemon.moves[1]}
+                      </span>
                     </div>
-                   
-
-                    <div className="stateBar">
-                      <ProgressBar
-                      poke={pokemon}
-                      stat={pokemon.hp}>
-                      </ProgressBar>
-                    </div>
-                  </div>
-                  
+                  )}
+                  <span className="categoryTitle">Moves</span>
                 </div>
-            </div>         
-      </div>
+              </div>
+            </div>
+
+            <div className="pokeSummary">
+              <span>{pokemon.description}</span>
+            </div>
+
+            <div className="pokeStats">
+              <div>
+                <span
+                  className={` subtitle2 ${
+                    typeof pokemon.type === "string"
+                      ? pokemon.type
+                      : pokemon.type[0]
+                  } backgroundWhite`}
+                >
+                  Base Stats
+                </span>
+              </div>
+              <div className="statsFlex">
+                <div className="nameAndNumber">
+                  <div className="stats">
+                    <span
+                      className={`${
+                        typeof pokemon.type === "string"
+                          ? pokemon.type
+                          : pokemon.type[0]
+                      } backgroundWhite`}
+                    >
+                      HP
+                    </span>
+                    <span
+                      className={`${
+                        typeof pokemon.type === "string"
+                          ? pokemon.type
+                          : pokemon.type[0]
+                      } backgroundWhite`}
+                    >
+                      ATK
+                    </span>
+                    <span
+                      className={`${
+                        typeof pokemon.type === "string"
+                          ? pokemon.type
+                          : pokemon.type[0]
+                      } backgroundWhite`}
+                    >
+                      DEF
+                    </span>
+                    <span
+                      className={`${
+                        typeof pokemon.type === "string"
+                          ? pokemon.type
+                          : pokemon.type[0]
+                      } backgroundWhite`}
+                    >
+                      SATK
+                    </span>
+                    <span
+                      className={`${
+                        typeof pokemon.type === "string"
+                          ? pokemon.type
+                          : pokemon.type[0]
+                      } backgroundWhite`}
+                    >
+                      SDEF
+                    </span>
+                    <span
+                      className={`${
+                        typeof pokemon.type === "string"
+                          ? pokemon.type
+                          : pokemon.type[0]
+                      } backgroundWhite`}
+                    >
+                      SPD
+                    </span>
+                  </div>
+                  <hr />
+                  <div className="stats">
+                    <span>{fixStats(pokemon.hp)}</span>
+                    <span>{fixStats(pokemon.atk)}</span>
+                    <span>{fixStats(pokemon.def)}</span>
+                    <span>{fixStats(pokemon.satk)}</span>
+                    <span>{fixStats(pokemon.sdef)}</span>
+                    <span>{fixStats(pokemon.spd)}</span>
+                  </div>
+                </div>
+
+                <div className="stateBar">
+                  <ProgressBar poke={pokemon} stat={pokemon.hp}></ProgressBar>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </React.Fragment>
   );
