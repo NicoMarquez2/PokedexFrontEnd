e3cbb77ebb09897df5126bc27f9cc108c9a173a1
ignacio gaston odella <108488609+Nano92OB@users.noreply.github.com>
2022-10-14 20:35:24 -0300

    TERMINADO SI SI

--- src/App.js
+++ src/App.js
@@ -2,78 +2,77 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import PokeList from "./Components/PokeList";
 import PokeCard from "./Components/PokeCard";
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [list, setList] = useState([]);
-  const [pokeToShow, setPokeToShow] = useState("")
+  const [pokeToShow, setPokeToShow] = useState("");
 
   useEffect(() => {
-    let aux =[]
-    async function fetchData(){
-      for(let i = 1; i <= 20; i++){
+    let aux = [];
+    async function fetchData() {
+      for (let i = 1; i <= 200; i++) {
         await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-        .then((response) => response.json())
-        .then((data) => {
-        aux.push(
-          {
-            "name":data.name.charAt(0).toUpperCase()+data.name.slice(1),
-            "image":data.sprites.other['official-artwork'].front_default,
-            "hp":data.stats[0].base_stat,
-            "atk":data.stats[1].base_stat,
-            "def":data.stats[2].base_stat,
-            "satk":data.stats[3].base_stat,
-            "sdef":data.stats[4].base_stat,
-            "spd":data.stats[5].base_stat,
-            "weigth":data.weight/10,
-            "heigth":data.height/10,
-            "moves":data.moves.slice(0,2).map((move)=> move.move.name),
-            "type":data.types.map((type)=> type.type.name),
-            "description": getDescription(data),
-            "id":data.id
-            }
-        )
-        console.log(aux)
-        });
-        
+          .then((response) => response.json())
+          .then((data) => {
+            getDescription(data).then((description) => {
+              console.log(description, "desc.");
+              aux.push({
+                name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
+                image: data.sprites.other["official-artwork"].front_default,
+                hp: data.stats[0].base_stat,
+                atk: data.stats[1].base_stat,
+                def: data.stats[2].base_stat,
+                satk: data.stats[3].base_stat,
+                sdef: data.stats[4].base_stat,
+                spd: data.stats[5].base_stat,
+                weigth: data.weight / 10,
+                heigth: data.height / 10,
+                moves: data.moves.slice(0, 2).map((move) => move.move.name),
+                type: data.types.map((type) => type.type.name),
+                description,
+                id: data.id,
+              });
+              console.log("aux", aux);
+              setList(aux);
+            });
+          });
       }
-      function getDescription(data){
-        fetch(data.species.url)
-        .then((response) => response.json())
-        .then((data2)=>{console.log(data2.flavor_text_entries[69].flavor_text);return(data2.flavor_text_entries[69].flavor_text)})
-      }  
-      setList(aux)
+      async function getDescription(data) {
+        // return new Promise((res, rej) => {
+        //   fetch(data.species.url)
+        //     .then((response) => response.json())
+        //     .then((data2) => {
+        //       console.log(data2.flavor_text_entries[69].flavor_text);
+        //       res(data2.flavor_text_entries[69].flavor_text);
+        //     });
+        // });
+        const resp = await fetch(data.species.url);
+        const data2 = await resp.json()
+        return data2.flavor_text_entries[69].flavor_text 
+
+      }
     }
-     fetchData()
+    fetchData();
   }, []);
 
-
-  function getPokemon(pokemon){
-    setPokeToShow(pokemon)
+  function getPokemon(pokemon) {
+    setPokeToShow(pokemon);
   }
 
   return (
     <React.Fragment>
-        <BrowserRouter>
-          <Routes>
-            <Route
+      <BrowserRouter>
+        <Routes>
+          <Route
             path="/"
-            element = {<PokeList list={list} 
-                      getPokemon={getPokemon}
-                      />}/>    
-            
-            <Route
-            path="/:id"
-            element = {<PokeCard
-            list={list}
-            />}/>                      
-          </Routes>         
-        </BrowserRouter>
-      </React.Fragment>
-      
+            element={<PokeList list={list} getPokemon={getPokemon} />}
+          />
+
+          <Route path="/:id" element={<PokeCard list={list} />} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
