1b8ed061ef85b38b3efbfac7ce0efa9c2e1ff0ea
Nicolas <nicolasmarquez005@gmail.com>
2022-12-12 20:36:40 -0300

    obtenemos valores de pokemon sin tipo y movimientos

--- src/Components/CreatePokemon.jsx
+++ src/Components/CreatePokemon.jsx
@@ -1,40 +1,121 @@
 import React, { useEffect, useState }  from "react";
 
 const CreatePokemon = () => {
+    
+    const [name, setName] = useState("")
+    const [img, setImg] = useState("")
+    const [weight, setWeight] = useState(0)
+    const [height, setHeight] = useState(0)
+    const [description, setDescription] = useState(0)
+    const [hp, setHp] = useState(0)
+    const [atk, setAtk] = useState(0)
+    const [def, setDef] = useState(0)
+    const [satk, setSatk] = useState(0)
+    const [sdef, setSdef] = useState(0)
+    const [spd, setSpd] = useState(0)
+
+
+     const handleName = (e) => {
+        e.preventDefault()
+        setName(e.target.value)
+      }
+
+      const handleImg = (e) => {
+        e.preventDefault()
+        setImg(e.target.value)
+      }
+
+      const handleWeight = (e) => {
+        e.preventDefault()
+        setWeight(e.target.value)
+      }
+
+      const handleHeight = (e) => {
+        e.preventDefault()
+        setHeight(e.target.value)
+      }
+
+      const handleDescription = (e) => {
+        e.preventDefault()
+        setDescription(e.target.value)
+      }
+
+      const handleHp = (e) => {
+        e.preventDefault()
+        setHp(e.target.value)
+      }
+      const handleAtk = (e) => {
+        e.preventDefault()
+        setAtk(e.target.value)
+      }
+      const handleDef = (e) => {
+        e.preventDefault()
+        setDef(e.target.value)
+      }
+      const handleSatk = (e) => {
+        e.preventDefault()
+        setSatk(e.target.value)
+      }
+      const handleSdef = (e) => {
+        e.preventDefault()
+        setSdef(e.target.value)
+      }
+      const handleSpd = (e) => {
+        e.preventDefault()
+        setSpd(e.target.value)
+      }
+      
+      const handleButton = async () => {
+        let pokemon = {
+            name: name,
+            img: img,
+            description: description,
+            weight: weight,
+            height: height,
+            hp: hp,
+            atk: atk,
+            def: def,
+            satk: satk,
+            sdef: sdef,
+            spd: spd
+        }
+        console.log(pokemon)
+      }
+
     return(
-        <form action="">
+        <form onSubmit={(e)=>e.preventDefault()}>
             <label htmlFor="name">Name</label>
-            <input name="name" type="text" />
+            <input name="Name" type="text" onChange={handleName}/>
 
             <label htmlFor="img">img</label>
-            <input name="img" type="text" />
+            <input name="img" type="text" onChange={handleImg}/>
 
             <label htmlFor="weight">weight</label>
-            <input name="weight" type="text" />
+            <input name="weight" type="text" onChange={handleWeight}/>
 
             <label htmlFor="height">height</label>
-            <input name="height" type="text" />
+            <input name="height" type="text" onChange={handleHeight}/>
 
             <label htmlFor="description">description</label>
-            <input name="description" type="text" />
+            <input name="description" type="text" onChange={handleDescription}/>
 
             <label htmlFor="hp">hp</label>
-            <input name="hp" type="text" />
+            <input name="hp" type="text" onChange={handleHp}/>
 
             <label htmlFor="atk">atk</label>
-            <input name="atk" type="text" />
+            <input name="atk" type="text" onChange={handleAtk}/>
 
             <label htmlFor="def">def</label>
-            <input name="def" type="text" />
+            <input name="def" type="text" onChange={handleDef}/>
             
             <label htmlFor="satk">satk</label>
-            <input name="satk" type="text" />
+            <input name="satk" type="text" onChange={handleSatk}/>
 
             <label htmlFor="sdef">sdef</label>
-            <input name="sdef" type="text" />
+            <input name="sdef" type="text" onChange={handleSdef}/>
 
             <label htmlFor="spd">spd</label>
-            <input name="spd" type="text" />
+            <input name="spd" type="text" onChange={handleSpd}/>
             <div>
                 <span>Select movements</span>
                 <select></select>
@@ -42,10 +123,12 @@ const CreatePokemon = () => {
             </div>
             <div>
                 <span>Select types</span>
-                <select></select>
+                <select>
+                    <option value="">TIPO</option>
+                </select>
                 <select></select>
             </div>
-            <button>Create</button>
+            <button type="button" onClick={handleButton}>Create</button>
         </form>
     )
 } 
