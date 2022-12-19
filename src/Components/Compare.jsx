08671067077d80c584ec3ee8e129befc9c1ee631
BadSh4dow <guidocabrera711@gmail.com>
2022-12-19 20:20:25 -0300

    dale que sale

--- src/Components/Compare.jsx
+++ src/Components/Compare.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ProgressBar from "./ProgressBar";
 
 
 const Compare = (props) => {
+
     const handleButton = (e) => {
         e.preventDefault()
         console.log(props.list)
     }
+    const [pokemon, setPokemon] = useState();
+
+const handleSelect = () => {
+            async function fetchData(){
+              await fetch('http://localhost:8080/pokemon/144')
+              .then((response)=>response.json())
+              .then((data) => {
+                let aux 
+                console.log(data.pokemon[0])
+                aux ={
+                  name: data.pokemon[0].name,
+                  image: data.pokemon[0].img,
+                  hp: data.pokemon[0].hp,
+                  atk: data.pokemon[0].atk,
+                  def: data.pokemon[0].def,
+                  satk: data.pokemon[0].satk,
+                  sdef: data.pokemon[0].sdef,
+                  spd: data.pokemon[0].spd,
+                  weigth: data.pokemon[0].weight,
+                  heigth: data.pokemon[0].height,
+                  moves: data.movements[0].filter(move => move.id_pokemon == data.pokemon[0].id).map(move => move.movement),
+                  type: data.types[0].filter(type => type.id_pokemon == data.pokemon[0].id).map(type => type.type),
+                  description: data.pokemon[0].description,
+                  id: data.pokemon[0].id,
+                }
+                setPokemon(aux)
+              })
+            } 
+        fetchData()
+        console.log("funciona por favor te pido marta")
+}
+
+function fixStats(pokeStats) {
+    if (pokeStats < 100) return "0" + pokeStats;
+    else return pokeStats;
+  }
+
+
 
     return(
         <div className="comparePokemon">
@@ -19,7 +59,7 @@ const Compare = (props) => {
             <div className="selection">
                 <div>
                     <label htmlFor="firstPokemon">Choose Pokemon</label>
-                    <select className="selectPokemon" name="firstPokemon">
+                    <select className="selectPokemon" name="firstPokemon" onChange={handleSelect}>
                         {props.list.map((element, key) => (
                             <option key={key} value={`${element.name}`}>{element.name}</option>
                         ))}
@@ -34,26 +74,94 @@ const Compare = (props) => {
                     </select>
                 </div>
             </div>
-            <div className="pokemons">
+            {pokemon && <div className="pokemons">
                 <div className="toCompare">
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
+                    <div className="pokeType">
+                        {pokemon.type.map((type, key) => (
+                        <span
+                        key={key}
+                        className={`types ${type} lettersWhite typeTitle`}
+                        >
+                        {type}
+                        </span>
+                        ))}
+                    </div>
+                    <div className="nameAndNumber">
+                        <div className="stats">
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            HP
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            ATK
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            DEF
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            SATK
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            SDEF
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            SPD
+                            </span>
+                        </div>
+                    <hr />
+                        <div className="stats">
+                            <span>{fixStats(pokemon.hp)}</span>
+                            <span>{fixStats(pokemon.atk)}</span>
+                            <span>{fixStats(pokemon.def)}</span>
+                            <span>{fixStats(pokemon.satk)}</span>
+                            <span>{fixStats(pokemon.sdef)}</span>
+                            <span>{fixStats(pokemon.spd)}</span>
+                        </div>
+                    </div>
                 </div>
                 <div className="toCompare">
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
-                    <p>POKEMON</p>
+                    <div className="pokeType">
+                        {pokemon.type.map((type, key) => (
+                        <span
+                        key={key}
+                        className={`types ${type} lettersWhite typeTitle`}
+                        >
+                        {type}
+                        </span>
+                        ))}
+                    </div>
+                    <div className="nameAndNumber">
+                        <div className="stats">
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            HP
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            ATK
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            DEF
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            SATK
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            SDEF
+                            </span>
+                            <span className={`${pokemon.type[0]} backgroundWhite`}>
+                            SPD
+                            </span>
+                        </div>
+                    <hr />
+                        <div className="stats">
+                            <span>{fixStats(pokemon.hp)}</span>
+                            <span>{fixStats(pokemon.atk)}</span>
+                            <span>{fixStats(pokemon.def)}</span>
+                            <span>{fixStats(pokemon.satk)}</span>
+                            <span>{fixStats(pokemon.sdef)}</span>
+                            <span>{fixStats(pokemon.spd)}</span>
+                        </div>
+                    </div>
                 </div>
-            </div>      
+            </div>}      
         </div>
     )
 } 
